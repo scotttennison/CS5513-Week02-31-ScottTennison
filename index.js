fee381ab32a2b9a9ccce9d770256db25cfcca84d
Scott Tennison <105877930+scotttennison@users.noreply.github.com>
2022-08-31 23:47:49 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+
+const http = require("http");
+const fs = require('fs').promises;
+
+const requestListener = function (req, res) {
+  
+  console.log(req.url);
+
+  if (req.url === "/") {
+    fs.readFile(__dirname + "/home.html")
+      .then(contents => {
+        res.setHeader("Content-Type", "text/html; charset=UTF-8");
+        res.writeHead(200);
+        res.end(contents);
+      });
+  } else {
+    fs.readFile(__dirname + "/stuff.json")
+      .then(contents => {
+        res.setHeader("Content-Type", "application/json; charset=UTF-8");
+        res.writeHead(200);
+        res.end(contents);
+      });
+
+  }
+  
+};
+
+const server = http.createServer(requestListener);
+
+const host = "0.0.0.0";
+const port = "8080";
+
+server.listen(
+  port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+  }
+);
+
+// non-arrow function syntax of same code as lines 51-56 above
+/*
+server.listen(port, host, function() {
+  console.log(`Server is running on http://${host}:${port}`);
+});
+*/
